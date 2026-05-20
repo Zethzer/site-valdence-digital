181970869d30d5857f1d780f73a8251cee272c21
Zethzer <bernard.yannick.pro@gmail.com>
2026-05-20 22:53:32 +0200

    chore: Update timestamp for a static Google review

--- src/data/reviews.ts
+++ src/data/reviews.ts
@@ -13,7 +13,7 @@ export const staticReviews: GoogleReview[] = [
     authorName: 'Jonathan Deymier',
     rating: 5,
     text: "Super travail, très professionnel avec une rapidité juste incroyable, beaucoup d'accessibilité de partage, un travail propre efficace que je recommande et dont j'ai décidé de poursuivre cette collaboration pour le développement de mon site internet. Encore un grand merci. Jonathan Deymier",
-    time: 1746057600,
+    time: 1777593600,
     profilePhotoUrl:
       'https://lh3.googleusercontent.com/a-/ALV-UjV4UitYuu1EShhj9PG0Hu08L3l7_Z1gTDifLYNZvcN-Im6FSZ3j=w72-h72-p-rp-mo-br100',
   },
